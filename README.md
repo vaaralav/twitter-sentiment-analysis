d61523c781686fa3560b98e74f41d4fffa3f2f9e
Ville Vaarala <ville.l.vaarala@gmail.com>
2017-05-21 22:53:49 +0700

    Support multiple search phrases for one execute

--- README.md
+++ README.md
@@ -13,11 +13,30 @@ yarn && yarn compile
 
 ## Usage
 
-`execute` script takes the search phrase as the first command line argument.
+`execute` script takes the search phrase as the first command line argument. You can provide up to 15 search phrases. Just separate them with a space (`' '`).
 
 ```shell
-yarn execute -- search_phrase_here
+yarn execute -- first_search_phrase [optional_search_phrases]
 ```
+
+### Example:
+
+```shell
+yarn execute -- "iphone\ 6" galaxys7 "oneplus\ 3t"
+yarn execute v0.17.10
+$ node build/index.js iphone\ 6 galaxys7 oneplus\ 3t
+See the results at 'out/5da15a08-e00d-4e8a-b1a2-4e2bc152f409.csv'
+✨  Done in 4.18s.
+```
+
+Results in the .csv file:
+
+| search_phrase | mean_sentiment_score | max_sentiment_score | min_sentiment_score | n   |
+| ------------- | -------------------- | ------------------- | ------------------- | --- |
+| iphone+6      |   0.5805778743242342 |                   6 |                  -2 |  77 |
+| galaxys7      |   0.2500014916782014 |                   9 |                  -1 |  96 |
+| oneplus+3t    |    4.000872629710595 |                   9 |                  -3 | 100 |
+
 
 ## Developing
 
--- src/index.js
+++ src/index.js
@@ -4,17 +4,40 @@ import fs from 'fs';
 import Twitter from 'twitter';
 import sentiment from 'sentiment';
 import csvWriter from 'csv-write-stream';
+import uuid from 'uuid';
 
 // Create a directory where to write results as csv
-fs.mkdirSync(process.cwd() + '/out');
+try {
+  fs.mkdirSync(process.cwd() + '/out');
+} catch(err) {
+  if (err.code !== 'EEXIST') {
+    throw err;
+  }
+}
 
 const SEARCH_PHRASE_INDEX = 2;
+const CSV_HEADERS = [
+  'search_phrase',
+  'mean_sentiment_score',
+  'max_sentiment_score',
+  'min_sentiment_score',
+  'n'
+];
+const INITIAL_ANALYSIS = {
+  mean: 0,
+  max: 0,
+  min: 0
+};
 
-const searchPhrase = process.argv[SEARCH_PHRASE_INDEX];
-
-if (searchPhrase === undefined) {
-  throw new Error('Please search something using a command line argument!');
+if (process.argv.length <= SEARCH_PHRASE_INDEX) {
+  throw new Error('Please search for something using a command line argument!');
 }
+if (process.argv.length > SEARCH_PHRASE_INDEX + 15) {
+  throw new Error('Search phrase count is restricted to 15!');
+}
+
+const searchPhrases = process.argv.slice(SEARCH_PHRASE_INDEX);
+const results = {};
 
 const twitter = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -23,22 +46,68 @@ const twitter = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-console.log(`Searching for ${searchPhrase}`);
-
-function getMeanSentimentScore(tweets) {
-  return tweets.statuses
-      .map(tweet => tweet.text)
-      .reduce((avgScore, text) => {
-        return (avgScore + sentiment(text).score)/2;
-      }, 0);
+function getMeanSentiment(texts) {
+  return texts.reduce((avgScore, text) => 
+    (avgScore + sentiment(text).score) / 2 ,0
+  );
 }
 
-twitter.get('search/tweets', {q: 'elmlang'}, function(error, tweets, response) {
-  const writer = csvWriter({
-    headers: ['search_phrase', 'mean_sentiment_score']
+function getSentimentAnalysis(texts) {
+  return texts.reduce(({mean, max, min}, text) => {
+    const score = sentiment(text).score;
+    return ({
+      mean: (mean + score) / 2,
+      min: min < score ? min : score,
+      max: max > score ? max : score
+    });
+  }, INITIAL_ANALYSIS)
+}
+
+function mapTweetsToScoreObject(tweets) {
+  const {statuses, search_metadata: {query}} = tweets;
+  const analysis = getSentimentAnalysis(statuses.map(({text}) => text));
+  
+  return ({
+    phrase: query,
+    n: statuses.length,
+    ...analysis
   });
-  writer.pipe(fs.createWriteStream(`out/${searchPhrase}.csv`));
-  writer.write([searchPhrase, getMeanSentimentScore(tweets)]);
+}
+
+function mapScoreObjectToCSVRow(score) {
+  return [
+    score.phrase,
+    score.mean,
+    score.max,
+    score.min,
+    score.n
+  ];
+}
+
+function writeScoresToFile(scores) {
+  const writer = csvWriter({
+    headers: CSV_HEADERS
+  });
+  const fileName = `${uuid()}.csv`;
+  writer.pipe(fs.createWriteStream(`out/${fileName}`));
+  scores.map(score => 
+    writer.write(mapScoreObjectToCSVRow(score))
+  );
   writer.end();
-  console.log(getMeanSentimentScore(tweets));
-});
+  return fileName;
+}
+
+// Where all the magic happens...
+Promise.all(
+  searchPhrases.map( phrase =>
+    twitter.get('search/tweets', {q: phrase, count: 100})
+  )
+).then( responses => {
+  return Promise.all(
+    responses.map((tweets) =>
+      mapTweetsToScoreObject(tweets)
+    ))
+}).then( scores => {
+  const fileName = writeScoresToFile(scores);
+  console.log(`See the results at 'out/${fileName}'`);
+}).catch(err => console.log(err));
